356e77dbcb965476094278ca4e66c15eb37c723e
LoganSweet <logan.sweet@students.olin.edu>
2015-03-30 14:55:05 -0400

    almost done toolbox

--- toolbox/geocoding_apis/mbta_finder.py
+++ toolbox/geocoding_apis/mbta_finder.py
@@ -1,4 +1,16 @@
 """
+
+UNFINISHED: The get j_son and get_lat_long functions are finished and working:
+I need to finishe the rest so that a place/ address input will return the 
+distnace and name off the closest MBTA stop. 
+
+
+
+
+
+
+
+
 Geocoding and Web APIs Project Toolbox exercise
 
 Find the MBTA stops closest to a given location.
@@ -10,6 +22,7 @@ https://sites.google.com/site/sd15spring/home/project-toolbox/geocoding-and-web-
 import urllib   # urlencode function
 import urllib2  # urlopen function (better than urllib version)
 import json
+from pprint import pprint
 
 
 # Useful URLs (you need to add the appropriate parameters for your requests)
@@ -18,14 +31,27 @@ MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
 MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 
 
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=Fenway%20Park"
+
 # A little bit of scaffolding if you want to use it
+
+def get_placeurl(place_name):
+
+
+    return 0
+
 
 def get_json(url):
     """
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    
+    f = urllib2.urlopen(url)
+    response_text = f.read()
+    response_data = json.loads(response_text)
+    pprint(response_data)
+    #print response_data["results"][0]["formatted_address"]
 
 
 def get_lat_long(place_name):
@@ -36,7 +62,18 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
-    pass
+    f = urllib2.urlopen(url)
+    #f = urllib2.urlopen(get_placeurl(place_name))
+    response_text = f.read()
+    data = json.loads(response_text)
+    #pprint(response_data)
+    latlon = data["results"][0]["geometry"]["location"]
+    #print latlon
+    #print type(latlon)
+    latlontup = (latlon[u'lat'], latlon[u'lng'])
+    #print latlontup
+    return latlontup
+
 
 
 def get_nearest_station(latitude, longitude):
@@ -56,4 +93,8 @@ def find_stop_near(place_name):
     distance from the given place to that stop.
     """
     pass
+
+
+get_lat_long(url)
+
 
